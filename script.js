6a25e99253332e38842f915376660149c0213daa
Semih TULGAR <semihtulgar@gmail.com>
2022-02-27 16:07:44 +0300

    Katılımcı formu bug'ı çözüldü. Kullanıcı kayıt olduğunda alert gösterilmesi sağlandı.

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let participants = [];
 // Kullanıcı Girdileri
 const form = document.getElementsByTagName("form")[0];
 
+const successAlert = document.getElementById("alert-box");
+successAlert.style.display = "none";
 
 let submitButton = document.getElementById("submitbtn");
 submitButton.addEventListener("click", AddParticipant);
@@ -22,18 +24,31 @@ function Participant(name, surname, email, phone, dateofbirth, gender, education
 
 
 function AddParticipant() {
-    const participant = new Participant(
-        form[0].value,
-        form[1].value,
-        form[2].value,
-        form[3].value,
-        form[4].value,
-        form[5].value,
-        form[6].value
-    );
 
-    participants.push(participant);
+    console.clear();
 
-    console.log(participants);
+    if (form.checkValidity()) {
+        const participant = new Participant(
+            form[0].value,
+            form[1].value,
+            form[2].value,
+            form[3].value,
+            form[4].value,
+            form[5].value,
+            form[6].value,
+            form[7].value
+        );
+
+        successAlert.style.display = "block";
+
+        setTimeout(() => {
+            successAlert.style.display = "none";
+        }, 1000);
+
+        participants.push(participant);
+
+        console.log(participants);
+    }
+
 
 }
